e42637cdfdc1d85848f67cdc3929f6a073d3f361
Ravi Teja Gunapati <ravitejagunapati.tosca@gmail.com>
2026-02-18 22:49:31 +0530

    Message

---
+++ assignments/Ravi_Assignment1.ts
@@ -0,0 +1,111 @@
+//Assignment 1 - Data Types
+
+//Question 1
+//Temperature of a city in degrees Celsius: 25.5
+
+let Temp:Number = 25.5;
+
+console.log("Temperature of a city in degrees Celsius:", Temp)
+console.log(`Temperature of a city in degrees Celsius: ${Temp}`)
+
+//Question 2
+//Whether a customer has placed an order: true or false
+
+let OrderStatus:Boolean = true;
+console.log(OrderStatus)
+console.log(`Whether a customer has placed an order:${OrderStatus}`);
+
+//Question 3
+//Person's phone number: "123-456-7890"
+
+let phno1:string = "123-456-7890";
+
+console.log(`Person's phone number: "${phno1}"`);
+
+let phno2:string = '"123-456-7890"';
+
+console.log(`Person's phone number: ${phno2}`);
+
+let phno3:string = '123-456-7890';
+
+console.log(`Person's phone number: "${phno3}"`);
+
+
+//Quesetion 4
+//Amount of money in a customer's bank account: 1000.50
+
+let Bal:number = 1000.50;
+
+console.log(`Amount of money in a customer's bank account: ${Bal}`);
+
+//Question 5
+//Person's email address: "john.doe@example.com"
+
+let email:string = "john.doe@example.com"
+
+console.log(`Person's email address: ${email}`)
+
+
+//Question6
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+let lati:number = 37.7749
+let Long:number = -122.4194
+
+console.log(`Coordinates of a location (latitude, longitude): ${lati},${Long}`)
+
+//Question7.
+//Person's marital status: true or false
+
+let 
+
+//Question8.
+//Person's occupation: "Software Engineer"
+
+
+//Question9.
+//Person's favourite colour: "Blue"
+
+
+//Question10.
+//Current year: 2023
+
+
+//Question11.
+//Number of followers on a social media platform: 1,000,000
+
+
+//Question12.
+//Rating of a movie: 7.5
+
+
+//Question13.
+//Person's blood type: 'A'
+
+
+//Question14.
+//Title of a book: "To Kill a Mockingbird"
+
+
+//Question15.
+//Number of employees in a company: 500
+
+
+//Question16.
+//Time of an event: 2:30 PM
+
+
+//Question17.
+//Name of a country: "United States"
+
+
+//Question18.
+//Person's eye color: "Brown"
+
+
+//Question19.
+//Person's birthplace: "New York City"
+
+
+//Question20.
+//Distance between two cities: 200.5
