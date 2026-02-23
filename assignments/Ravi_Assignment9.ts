5bfd99824da5e26e02faf02be5b99a519be5ca8c
Gunapati <sriteja6@deloitte.com>
2026-02-24 00:42:09 +0530

    Ravi_Assignment9_Submission

---
+++ assignments/Ravi_Assignment9.ts
@@ -0,0 +1,20 @@
+//Right side triangle with *
+
+
+let stars: number = 5;
+for (let i:number = 1; i <=stars; i++) 
+{
+    let print: string = " ";
+
+    for (let j:number = i; j <=stars; j++)
+    { 
+        print += " ";
+    }
+    
+            for(let k:number = 1; k<=i; k++)
+            {
+                print += "*";
+            }
+        console.log(print);
+
+}
